ecbbfa25c77ff1072e0ba46d7bfe2d0558a33f62
Danielle <dani.camp1125@gmail.com>
2025-03-03 20:08:48 -0500

    steps 6 and 7 completed

--- index.js
+++ index.js
@@ -10,46 +10,45 @@
 //3.Create a prompt asking for their favorite flavor using list of integers
 const userInputString = prompt("what is your favorite flavor?", "vanilla, vanilla, vanilla, strawberry, coffee, coffee")
 
-//4.Split the string of numbers into an array of strings
-const stringArray = userInputString.split(",");
-
-const flavors = {
-    vanilla: 3,
-    coffee: 2,
-    strawberry: 1,
-};
-
-console.log(Object.keys(flavors));
-console.table(Object.keys(flavors));
-
-console.log(Object.values(flavors));
-console.table(Object.values(flavors));
-
-console.log(flavors);
-console.table(flavors);
+//4.Split the string of numbers into an array of strings. Make a table so you can see the keys and values side by side.
+const flavor = userInputString.split(",");
+console.log(flavor);
+console.table(flavor);
 
 //5. Loop is used to iterate through the array of flavors
-for(let i=0; i<flavors.length;i++){
-    console.log("current index", i, "current value", flavors[i])
-};
-
+for (const key in flavor){
+    console.log(key)
+    console.log(flavor[key])
+}
 
 console.log("_________");
 
-let idx=0
-while(idx<flavors.length){
-    console.log(flavors[idx])
-    idx++
-};
-
-//6. An Objects is used too keep count of how many orders there are of each flavor. 
+//6. An Object is used to keep count of how many orders there are of each flavor. 
 
 function printObject (obj){
-    for(const key in obj){
+    for (const key in obj){
         console.log(`${key}: ${obj[key]}`)
     }
 };
-printObject(flavors);
+printObject(flavor);
 
-//The program counts the number of each flavor in the user's input. The logic for counting the frequencies of elements in an array is organized into a function that returns an object. Console output changes based upon user's input.
+console.log("________")
+
+//7.  Number of elements
+function countElements(flavor){
+    let count = 0;
+    for(let i = 0; i < flavor.length; i++){
+        count++;
+    }
+    return count;
+}
+const myFlavor = ["vanilla", "strawberry", "coffee"];
+const elementCount = countElements(myFlavor);
+console.log("Number of elements:", elementCount);
+
+
+
+
+
+
 
